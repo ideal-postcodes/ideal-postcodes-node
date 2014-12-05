7cc18e52b02bc5cc2e6679e449dc9a1af80e1305
cblanc <cablanchard@gmail.com>
2014-12-05 06:48:15 +0000

    Renamed test folder

--- tests/postcode.js
+++ tests/postcode.js
@@ -13,17 +13,6 @@ module.exports = function (client) {
 				done();
 			});
 		});
-
-
-		// Only works with paid up token
-		// it ("should provide an empty array if no match found", function (done) {
-		// 	client.lookupPostcode("BOGUS", function (error, result) {
-		// 		assert.isArray(result);
-		// 		assert.isNull(error);
-		// 		assert.equal(result.length, 0);
-		// 		done();
-		// 	})
-		// });	
 	});
 }
 
