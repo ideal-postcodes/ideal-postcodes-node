8afc255417d126bf7a911c23e595e964c90daa14
cblanc <cablanchard@gmail.com>
2014-12-09 17:02:56 +0000

    Fix nock

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,9 +1,11 @@
 "use strict";
 
+var nock = require("nock");
+
 if (process.env.LIVE) {
-	var server = require("./mock_server.js");
+	require("./mock_server.js");
 } else {
-	var nock = require("./http_mocking.js");
+	require("./http_mocking.js");
 }
 
 if (process.env.RECORD) {
